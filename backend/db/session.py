1cf1ae953601a2bb3118582b3637f3423def63e5
Oscar <oscar_li916@yahoo.com.hk>
2023-02-25 15:10:57 +0800

    test: test cases for crud rbac

--- backend/db/session.py
+++ backend/db/session.py
@@ -13,3 +13,9 @@ engine = create_engine(
     pool_pre_ping=True,
 )
 SessionLocal = sessionmaker(bind=engine)
+
+test_engine = create_engine(
+    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_TEST_PORT')}/{os.getenv('POSTGRES_TEST_DB')}",
+    pool_pre_ping=True,
+)
+TestSessionLocal = sessionmaker(bind=test_engine)
