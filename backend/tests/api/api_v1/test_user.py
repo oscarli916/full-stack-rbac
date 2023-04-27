69909fda3f701c1ad04999fe82d274559c7a7e5a
Oscar <oscar_li916@yahoo.com.hk>
2023-04-27 09:40:54 +0800

    test: add test case for GET user endpoint

--- backend/tests/api/api_v1/test_user.py
+++ backend/tests/api/api_v1/test_user.py
@@ -74,3 +74,31 @@ def test_create_duplicate_user(db: Session) -> None:
     assert "message" in res
     assert "error" in res
     assert res["message"] == "user has already been created"
+
+
+def test_read_users(db: Session) -> None:
+    email = "admin@test.com"
+    password = "12345678"
+    permissions = ["setting.create", "setting.read", "setting.update", "setting.delete"]
+    admin = UserCreate(email=email, password=password)
+    user = crud.rbac.create_user(db, obj_in=admin)
+    role = crud.rbac.create_role(db, role_name="admin")
+    crud.rbac.create_user_has_role(db, user_id=user.id, role_id=role.id)
+    db_objs = crud.rbac.create_permissions(db, permissions=permissions)
+    crud.rbac.create_role_has_permission(
+        db, role_id=role.id, permission_ids=[obj.id for obj in db_objs]
+    )
+
+    login_data = {"email": email, "password": password}
+    r = client.post("/api/v1/auth/login", json=login_data)
+    res = r.json()
+
+    header = {"authorization": f"Bearer {res['token']}"}
+    r = client.get("/api/v1/rbac/user", headers=header)
+    res = r.json()
+    assert r.status_code == 200
+    assert len(res) == 1
+    assert "id" in res[0]
+    assert "email" in res[0]
+    assert res[0]["id"] == str(user.id)
+    assert res[0]["email"] == email
