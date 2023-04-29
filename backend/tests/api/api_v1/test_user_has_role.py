2bb4b1d17ab0a2992e5e96ec4d678131a4b0e67d
Oscar <oscar_li916@yahoo.com.hk>
2023-04-29 20:49:43 +0800

    test: add test case for DELETE user has role endpoint

--- backend/tests/api/api_v1/test_user_has_role.py
+++ backend/tests/api/api_v1/test_user_has_role.py
@@ -315,3 +315,53 @@ def test_update_not_found_user_has_role(db: Session) -> None:
     assert "error" in res
     assert res["message"] == "user has role not found"
     assert res["error"] == f"no user id: {user.id}, role id: {uuid}"
+
+
+def test_delete_not_found_user_has_role(db: Session) -> None:
+    email = "admin@test.com"
+    password = "12345678"
+    permissions = ["setting.create", "setting.read", "setting.update", "setting.delete"]
+    admin = UserCreate(email=email, password=password)
+    user = crud.rbac.create_user(db, obj_in=admin)
+    role = crud.rbac.create_role(db, role_name="admin")
+    crud.rbac.create_user_has_role(db, user_id=user.id, role_id=role.id)
+    db_objs = crud.rbac.create_permissions(db, permissions=permissions)
+    crud.rbac.create_role_has_permission(
+        db, role_id=role.id, permission_ids=[obj.id for obj in db_objs]
+    )
+
+    login_data = {"email": email, "password": password}
+    r = client.post("/api/v1/auth/login", json=login_data)
+    res = r.json()
+
+    uuid = uuid4()
+    header = {"authorization": f"Bearer {res['token']}"}
+    r = client.delete(f"/api/v1/rbac/user-has-role/{uuid}/{role.id}", headers=header)
+    res = r.json()
+    assert r.status_code == 404
+    assert "message" in res
+    assert "error" in res
+    assert res["message"] == "user has role not found"
+    assert res["error"] == f"no user id: {uuid}, role id: {role.id}"
+
+
+def test_delete_user_has_role(db: Session) -> None:
+    email = "admin@test.com"
+    password = "12345678"
+    permissions = ["setting.create", "setting.read", "setting.update", "setting.delete"]
+    admin = UserCreate(email=email, password=password)
+    user = crud.rbac.create_user(db, obj_in=admin)
+    role = crud.rbac.create_role(db, role_name="admin")
+    crud.rbac.create_user_has_role(db, user_id=user.id, role_id=role.id)
+    db_objs = crud.rbac.create_permissions(db, permissions=permissions)
+    crud.rbac.create_role_has_permission(
+        db, role_id=role.id, permission_ids=[obj.id for obj in db_objs]
+    )
+
+    login_data = {"email": email, "password": password}
+    r = client.post("/api/v1/auth/login", json=login_data)
+    res = r.json()
+
+    header = {"authorization": f"Bearer {res['token']}"}
+    r = client.delete(f"/api/v1/rbac/user-has-role/{user.id}/{role.id}", headers=header)
+    assert r.status_code == 204
