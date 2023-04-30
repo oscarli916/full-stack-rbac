54d79cae2339b0e2f12ae4cf9e7170cdedafe2d2
Oscar <oscar_li916@yahoo.com.hk>
2023-04-30 15:45:45 +0800

    refactor: move verify permission to auth to remove circular import

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -4,12 +4,8 @@ from typing import Any
 from uuid import UUID
 
 import bcrypt
-from fastapi.responses import JSONResponse
 import jwt
-from sqlalchemy.orm import Session
 
-from api.api_v1.endpoints import auth
-from schemas.token import Token
 from utils.exception import UvicornException
 
 
@@ -51,23 +47,3 @@ def verify_jwt(token: str) -> dict[str, Any]:
             message="user is not authorized",
             error=str(e),
         )
-
-
-async def verify_permission(
-    db: Session, authorization: str | None, permissions: list[str]
-) -> bool:
-    if not authorization or not authorization.startswith("Bearer "):
-        raise UvicornException(
-            status_code=401,
-            message="user is not authorized",
-            error="header does not start with Bearer",
-        )
-    token = authorization.split("Bearer ")[1]
-    res = await auth.auth(Token(permissions=permissions, token=token), db=db)
-    if type(res) == JSONResponse and res.status_code == 401:
-        raise UvicornException(
-            status_code=401,
-            message="user is not authorized",
-            error="user does not have permission",
-        )
-    return True
