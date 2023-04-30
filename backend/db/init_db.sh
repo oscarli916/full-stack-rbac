2b6c7b586f20b5ae90afc935f650047d2bab2c90
Oscar <oscar_li916@yahoo.com.hk>
2023-04-30 15:46:16 +0800

    chore: change seed normal user email

--- backend/db/init_db.sh
+++ backend/db/init_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 alembic upgrade head
 python seed_superuser.py admin@test.com 12345678
-python seed_user.py manager1@test.com 12345678
-python seed_user.py senior1@test.com 12345678
-python seed_user.py test1@test.com 12345678
+python seed_user.py manager@test.com 12345678
+python seed_user.py senior@test.com 12345678
+python seed_user.py test@test.com 12345678
 python seed_permission.py setting.create setting.read setting.update setting.delete
